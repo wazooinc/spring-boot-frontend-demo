a1dc6b06548f993222b8da31a6cee148ab7fc33f
Erik Yuzwa <erikyuzwa@gmail.com>
2022-07-08 18:17:18 -0600

    add thymeleaf root

---
+++ src/main/java/com/example/springbootfrontenddemo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.springbootfrontenddemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "index";
+    }
+}
