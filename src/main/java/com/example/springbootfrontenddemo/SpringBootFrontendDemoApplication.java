9b3b0cb59efc32163c8a60fce8f8668f38e25b75
Erik Yuzwa <erikyuzwa@gmail.com>
2022-07-08 18:12:05 -0600

    initial commit of spring framework

---
+++ src/main/java/com/example/springbootfrontenddemo/SpringBootFrontendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfrontenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFrontendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFrontendDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootfrontenddemo/SpringBootFrontendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootfrontenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFrontendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
